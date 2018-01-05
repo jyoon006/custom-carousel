77d0888cc2440467f72ad5ccf268769c231116c2
jyoon006 <jyoon006@gmail.com>
2018-01-05 10:17:43 -0800

    add image width/height option and showImages option to display exact number of images in carousel

--- example/example.jsx
+++ example/example.jsx
@@ -5,21 +5,22 @@ import ArtCarousel from '../src/index.jsx';
 class Example extends Component {
     render() {
         let options = {
-            carouselHeight: 300,
-            carouselWidth: 800,
-            showImages: 3
+            carouselWidth: 1800,
+            imageHeight: 250,
+            imageWidth: 400,
+            showImages: 4
         };
 
         return (
             <div>
                 <h2>Carousel Example</h2>
                 <ArtCarousel { ...options }>
-                    <div><img src='http://cdn.akc.org/Marketplace/Breeds/Rottweiler_SERP.jpg' /></div>
-                    <div><img src='http://cdn.akc.org/Marketplace/Breeds/Rottweiler_SERP.jpg' /></div>
-                    <div><img src='http://cdn.akc.org/Marketplace/Breeds/Rottweiler_SERP.jpg' /></div>
-                    <div><img src='http://cdn.akc.org/Marketplace/Breeds/Rottweiler_SERP.jpg' /></div>
-                    <div><img src='http://cdn.akc.org/Marketplace/Breeds/Rottweiler_SERP.jpg' /></div>
-                    <div><img src='http://cdn.akc.org/Marketplace/Breeds/Rottweiler_SERP.jpg' /></div>
+                    <div><img src='https://i.ytimg.com/vi/i3Pc_P1_bT0/maxresdefault.jpg' /></div>
+                    <div><img src='https://i.ytimg.com/vi/5yIccPnhvy0/maxresdefault.jpg' /></div>
+                    <div><img src='https://cdn-images-1.medium.com/max/1242/1*3fxwk_21oPeikN2q4siUpg@2x.jpeg' /></div>
+                    <div><img src='https://i.ytimg.com/vi/5yIccPnhvy0/maxresdefault.jpg' /></div>
+                    <div><img src='https://i.ytimg.com/vi/5yIccPnhvy0/maxresdefault.jpg' /></div>
+                    <div><img src='https://i.ytimg.com/vi/5yIccPnhvy0/maxresdefault.jpg' /></div>
                 </ArtCarousel>
                     
             </div>
--- src/index.jsx
+++ src/index.jsx
@@ -12,18 +12,22 @@ class Carousel extends Component {
 
     render() { 
         let styles = {
-            height: `${this.props.carouselHeight}px`,
-            width: `${this.props.carouselWidth}px`
+            height: typeof this.props.carouselHeight === 'number' ? `${this.props.carouselHeight}px`: this.props.carouselHeight || 'auto',
+            width: typeof this.props.carouselWidth === 'number'? `${this.props.carouselWidth}px`: this.props.carouselWidth || '90vw'
         };
 
-        let imageWidth = this.props.carouselWidth / this.props.showImages;
-
+        if( this.props.showImages > 0 ) {
+            if(this.props.imageWidth) styles.width = this.props.imageWidth * this.props.showImages;
+            else styles.width = 500 * this.props.showImages;
+        }
+        // let imageWidth = this.props.carouselWidth / this.props.showImages;
+        console.log('asdfsad', this.props.imageWidth)
         const changeImageWidth = ( children ) => {
             return React.Children.map(children, (childNode) => {
-                console.log('children', children, 'childNode', typeof childNode)
                 return React.cloneElement(childNode, { 
                     style: {
-                        width: `${imageWidth}px`
+                        width: this.props.imageWidth ? `${this.props.imageWidth}px` : '500px',
+                        height: this.props.imageHeight ? `${this.props.imageHeight}px` : '500px'
                     }
                 });
             });
@@ -31,6 +35,7 @@ class Carousel extends Component {
 
         return (
             <div id="carousel-container" style={ styles }>
+                
                 <div className="carousel-inside-container" style= { styles }>
                    { changeImageWidth(this.props.children) }
                 </div>
