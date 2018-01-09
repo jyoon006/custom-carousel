ab6cf7d7e3ada026d42edf517ab93ccf4d209af4
jyoon006 <jyoon006@gmail.com>
2018-01-09 11:06:48 -0800

    implement mouse drag for carousel movement

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,98 @@
 import React, { Component } from 'react';
 import './index.css';
 import rightArrowSVG from './svg/Down-Arrow.svg';
+import _debounce from './debounce';
 
 class Carousel extends Component {
     constructor() {
         super();
         this.handleClick = this.handleClick.bind(this);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.mousemoveListener = this.mousemoveListener.bind(this);
+        this.mouseupListener = this.mouseupListener.bind(this);
+        this.captureMouseEvents = this.captureMouseEvents.bind(this);
         this.state = {
-            currentShiftedPosition: 0
+            currentShiftedPosition: 0,
+            dragStyle: '-webkit-grab',
+            previousMovementX: 0,
+            mouseDown: false
         };
     }
     componentDidMount() {
-        console.log(this)
-        // document.querySelector('#carousel-container').addEventListener('mousedown', (event) => {
-        //     console.log('EVENT', event)
+        // let mouseIsDown = false;
+        // document.querySelector('#carousel-container').addEventListener('mousedown', this.mousemoveListener);
+
+            
+    
+
+        // document.querySelector('#carousel-container').addEventListener('mouseup', (event) => {
+        //     this.setState({ mouseIsDown: false });
         // });
+    }
+    
+
+
+    mousemoveListener(event) {
+
+
+        const carouselWidth = document.querySelector('#carousel-container').offsetWidth;
+        const imageWidth = Number(document.querySelector('.carousel-inside-container div').style.width.slice(0, -2));
+        const imageDivWidth = imageWidth * this.props.children.length;
+        const maxImagesToShow = carouselWidth / imageWidth;
+
+
+        event.preventDefault();
+        if(this.state.mouseDown) this.setState({ previousMovementX: event.layerX });
+
+        
+        // if(this.state.currentShiftedPosition === 0) {
+        if(this.state.previousMovementX - event.layerX > 0 && this.state.currentShiftedPosition > -imageDivWidth + Math.floor(maxImagesToShow) * imageWidth) {
+            this.setState({ currentShiftedPosition: this.state.currentShiftedPosition - 15}, () => {
+                document.querySelector('.carousel-inside-container').style.transform = `translateX(${this.state.currentShiftedPosition}px)`;
+                document.querySelector('.carousel-inside-container').style.transition = '0.1s linear'; 
+            })
+                
+        } else if(this.state.previousMovementX - event.layerX < 0 && this.state.currentShiftedPosition < 0) {
+            this.setState({ currentShiftedPosition: this.state.currentShiftedPosition + 15}, () => {
+                document.querySelector('.carousel-inside-container').style.transform = `translateX(${this.state.currentShiftedPosition}px)`;
+                document.querySelector('.carousel-inside-container').style.transition = '0.1s linear'; 
+            })
+        }
+           
+            
+            
+        if(this.state.mouseDown) this.setState({ mouseDown: false });     
+                
+
+            // this.setState({ currentShiftedPosition: });
+        // }
+
+            
+        
+        
+        
+    }
+
+    mouseupListener(event) {
+        console.log('MOUSE UP')
+        event.stopPropagation();
+        document.removeEventListener('mouseup', this.mouseupListener, true);
+        document.querySelector('.carousel-inside-container').removeEventListener('mousemove', this.mousemoveListener, true);
+    }
+
+    captureMouseEvents() {
+        document.addEventListener('mouseup', this.mouseupListener, true);
+        document.querySelector('.carousel-inside-container').addEventListener('mousemove', this.mousemoveListener, true);
+    }
+
+    handleMouseDown(event) { 
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({mouseDown: true});
+            this.captureMouseEvents();
+        
+        
+        
     }
 
     changeImageWidth(children) {
@@ -40,7 +118,7 @@ class Carousel extends Component {
     handleTransform(target, shift) {
         
         let maxShift = null;
-        const carouselWidth = Number(document.querySelector('#carousel-container').style.width.slice(0, -2));
+        const carouselWidth = document.querySelector('#carousel-container').offsetWidth;
         const imageWidth = Number(document.querySelector('.carousel-inside-container div').style.width.slice(0, -2));
         const imageDivWidth = imageWidth * this.props.children.length;
         const maxImagesToShow = carouselWidth / imageWidth;
@@ -48,7 +126,7 @@ class Carousel extends Component {
 
         if(shift === 'negative') shift = -(Math.floor(maxImagesToShow) * imageWidth);
         else if(shift === 'positive') shift = Math.floor(maxImagesToShow) * imageWidth;
-        
+
         if(shift < 0) {
             if(this.state.currentShiftedPosition + (shift * 2) > -imageDivWidth) {
                 this.setState({ currentShiftedPosition: this.state.currentShiftedPosition + shift }, () => {
@@ -57,6 +135,7 @@ class Carousel extends Component {
                 });
             } else {
                 maxShift = -(imageDivWidth - (maxImagesToShow * imageWidth));
+                console.log('maxshift', maxShift)
                 this.setState({ currentShiftedPosition: maxShift }, () => {
                     document.querySelector(target).style.transform = `translateX(${this.state.currentShiftedPosition}px)`;
                     document.querySelector(target).style.transition = '0.2s ease-in';
@@ -80,22 +159,23 @@ class Carousel extends Component {
     }
 
     render() { 
-        let styles = {
+        let carouselStyles = {
             height: typeof this.props.carouselHeight === 'number' ? `${this.props.carouselHeight}px`: this.props.carouselHeight || 'auto',
             width: typeof this.props.carouselWidth === 'number'? `${this.props.carouselWidth}px`: this.props.carouselWidth || '90vw'
         };
 
-        if( this.props.showImages > 0 ) {
-            if(this.props.imageWidth) styles.width = this.props.imageWidth * this.props.showImages;
-            else styles.width = 500 * this.props.showImages;
+        if (this.props.showImages > 0) {
+            if(this.props.imageWidth) carouselStyles.width = this.props.imageWidth * this.props.showImages;
+            else carouselStyles.width = 500 * this.props.showImages;
         }
+
 
 
         return (
             <div id="main-container">
                 <div title="chevron-left" className= "chevron-left" onClick={ this.handleClick } dangerouslySetInnerHTML={{ __html: rightArrowSVG }}></div>
-                <div id="carousel-container" style={ styles }>
-                    <div className="carousel-inside-container" style= { styles }>
+                <div id="carousel-container" style={ carouselStyles } onMouseDown={ this.handleMouseDown }>
+                    <div className="carousel-inside-container" style={{cursor: this.state.dragStyle}}>
                     { this.changeImageWidth(this.props.children) }
                     </div>
                 </div>
